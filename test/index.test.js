69fedd13b1a0c8453150cc88a76a94779ee8794d
cbartram <cbartram3@gmail.com>
2020-04-13 13:51:47 -0400

    Added additional unit tests for more coverage

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { describe, it, beforeEach } = require('mocha');
+const { describe, it, beforeEach, afterEach } = require('mocha');
 const { expect } = require('chai');
 const nock = require('nock');
 const Nest = require('../src/index');
@@ -211,7 +211,18 @@ describe('Nest Camera Tests', () => {
 });
 
 describe('Mocking Camera API Tests', () => {
+  afterEach(() => nock.cleanAll());
   beforeEach(() => {
+    nock('https://oauth2.googleapis.com')
+      .post('/token')
+      .reply(200, {
+        access_token: 'ya29.a0Ae4lvC2w1OP-k2mBo9ebdW-GGCLoA1EW6s8AUgh7wLH',
+        expires_in: 3599,
+        scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/nest-account openid https://www.googleapis.com/auth/userinfo.email',
+        token_type: 'Bearer',
+        id_token: 'eyJMiLCJlbWFpbX25hbWUiOiJCYXJ0cmFtIiwibG9jYWxlIjoiZWAA',
+      });
+
     nock('https://nestauthproxyservice-pa.googleapis.com')
       .post('/v1/issue_jwt')
       .reply(200, {
@@ -230,5 +241,182 @@ describe('Mocking Camera API Tests', () => {
           structureConstraint: {},
         },
       });
+    nock('https://nexusapi-us1.dropcam.com')
+      .get('/cuepoint/foo/2')
+      .reply(200, [
+        {
+          playback_time: 1586795004200,
+          start_time: 1586795004200,
+          camera_uuid: 'some_id',
+          face_id: '',
+          is_important: false,
+          face_category: 'UNSET',
+          end_time: 1586795023650,
+          importance: 0,
+          face_name: '',
+          in_progress: false,
+          id: 'some-labs',
+          zone_ids: [],
+          types: [
+            'sound',
+          ],
+        },
+        {
+          playback_time: 1586795027720,
+          start_time: 1586795027720,
+          camera_uuid: '92b207f519f248409f09e60dfc6853e0',
+          face_id: '',
+          is_important: false,
+          face_category: 'UNSET',
+          end_time: 1586795036910,
+          importance: 0,
+          face_name: '',
+          in_progress: false,
+          id: '1586795027-labs',
+          zone_ids: [],
+          types: [
+            'sound',
+          ],
+        },
+      ]);
+  });
+
+  it('Throws an error when trying to unsubscribe from invalid type', (done) => {
+    const nest = new Nest({
+      nestId: 'foo', // Must be foo to satisfy nock URL
+      refreshToken: 'foo',
+      clientId: 'foo',
+      apiKey: 'foo',
+      eventInterval: 500,
+    });
+
+    nest.subscribe('event', () => {});
+    try {
+      nest.unsubscribe('invalid');
+    } catch (err) {
+      expect(err.message).to.be.a('string').that.equals('You must specify a type of event to unsubscribe from either: "event" or "snapshot"');
+      done();
+    }
+  });
+
+  it('Unsubscribes from the correct event type', (done) => {
+    const nest = new Nest({
+      nestId: 'foo', // Must be foo to satisfy nock URL
+      refreshToken: 'foo',
+      clientId: 'foo',
+      apiKey: 'foo',
+      eventInterval: 500,
+    });
+
+    nest.subscribe('event', () => {});
+    try {
+      nest.unsubscribe('event');
+      expect(nest._subscribedEventsObservable.Subscriber.closed).to.be.a('boolean').that.equals(true);
+      expect(nest._subscribedEventsObservable.Subscriber.isStopped).to.be.a('boolean').that.equals(true);
+    } catch (err) {
+      done();
+    }
+  });
+
+  it('Unsubscribes from the correct snapshot type', (done) => {
+    const nest = new Nest({
+      nestId: 'foo', // Must be foo to satisfy nock URL
+      refreshToken: 'foo',
+      clientId: 'foo',
+      apiKey: 'foo',
+      eventInterval: 500,
+    });
+
+    nest.subscribe('snapshot', () => {});
+    try {
+      nest.unsubscribe('snapshot');
+      expect(nest._subscribedSnapshotObservable.Subscriber.closed).to.be.a('boolean').that.equals(true);
+      expect(nest._subscribedSnapshotObservable.Subscriber.isStopped).to.be.a('boolean').that.equals(true);
+    } catch (err) {
+      done();
+    }
+  });
+
+  it('Retrieves a set of events from the Nest endpoint', async () => {
+    const nest = new Nest({
+      nestId: 'foo', // Must be foo to satisfy nock URL
+      refreshToken: 'foo',
+      clientId: 'foo',
+      apiKey: 'foo',
+    });
+
+    await nest.init();
+
+    const events = await nest.getEvents();
+    expect(events).to.be.a('array');
+    expect(events[0]).to.be.a('object');
+    expect(events[0]).to.deep.equal({
+      playback_time: 1586795004200,
+      start_time: 1586795004200,
+      camera_uuid: 'some_id',
+      face_id: '',
+      is_important: false,
+      face_category: 'UNSET',
+      end_time: 1586795023650,
+      importance: 0,
+      face_name: '',
+      in_progress: false,
+      id: 'some-labs',
+      zone_ids: [],
+      types: [
+        'sound',
+      ],
+    });
+  });
+
+  it('Returns null for getEvents() when an error is thrown', async () => {
+    nock.cleanAll();
+    nock('https://nexusapi-us1.dropcam.com')
+      .get('/cuepoint/foo/2')
+      .replyWithError('Failed to retrieve events');
+
+    nock('https://oauth2.googleapis.com')
+      .post('/token')
+      .reply(200, {
+        access_token: 'ya29.a0Ae4lvC2w1OP-k2mBo9ebdW-GGCLoA1EW6s8AUgh7wLH',
+        expires_in: 3599,
+        scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/nest-account openid https://www.googleapis.com/auth/userinfo.email',
+        token_type: 'Bearer',
+        id_token: 'eyJMiLCJlbWFpbX25hbWUiOiJCYXJ0cmFtIiwibG9jYWxlIjoiZWAA',
+      });
+
+    nock('https://nestauthproxyservice-pa.googleapis.com')
+      .post('/v1/issue_jwt')
+      .reply(200, {
+        jwt: 'g.0.eyJraWQiOiIyMzhiNTUxZmM',
+        claims: {
+          subject: {
+            nestId: {
+              namespaceId: {
+                id: 'nest-phoenix-prod',
+              },
+              id: '102568',
+            },
+          },
+          expirationTime: '2020-04-11T23:56:59.861Z',
+          policyId: 'authproxy-oauth-policy',
+          structureConstraint: {},
+        },
+      });
+
+    const nest = new Nest({
+      nestId: 'foo', // Must be foo to satisfy nock URL
+      refreshToken: 'foo',
+      clientId: 'foo',
+      apiKey: 'foo',
+    });
+
+    await nest.init();
+
+    try {
+      await nest.getEvents();
+    } catch (err) {
+      expect(err.message).to.be.a('string').that.equals('Error: Failed to retrieve events');
+    }
   });
 });
